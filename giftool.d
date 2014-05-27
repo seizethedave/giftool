d05a7d5680376beb1ad0a89a9f33d6fd8e642db5
David Grant <seizethedave@gmail.com>
2014-05-27 08:27:29 -0700

    Reading image data. And other stuff in a saner way.

--- giftool.d
+++ giftool.d
@@ -8,10 +8,27 @@ enum GifBlockType
 {
    Header,
    GlobalColorTable,
-   GraphicsControlExtension
+   GraphicsControlExtension,
+   ApplicationExtension,
+   CommentExtension,
+   PlainTextExtension,
+   TableBasedImage,
 }
 
+enum ubyte ImageSeparator = 0x2C;
+enum ubyte ExtensionIntroducer = 0x21;
+enum ubyte GraphicControlLabel = 0xF9;
+enum ubyte CommentLabel = 0xFE;
+enum ubyte PlainTextLabel = 0x01;
+enum ubyte ApplicationExtensionLabel = 0xFF;
+enum ubyte TrailerByte = 0x3B;
+
 alias void function(GifBlockType, size_t, size_t, void*) GifCallback;
+
+uint ColorCount(ubyte tableSize)
+{
+   return 2 ^^ (tableSize + 1);
+}
 
 struct HeaderBlock
 {
@@ -27,15 +44,9 @@ struct HeaderBlock
    ubyte colorResolution;
    ubyte sortFlag;
    ubyte colorTableSize;
-
-   @property
-   public uint GlobalColorCount()
-   {
-      return 2 ^^ (this.colorTableSize + 1);
-   }
 }
 
-struct GlobalColorTableBlock
+struct ColorTableBlock
 {
    ubyte[] table;
 }
@@ -56,11 +67,45 @@ struct GraphicsControlExtensionBlock
    ubyte blockTerminator;
 }
 
+struct ApplicationExtensionBlock
+{
+   ubyte extensionIntroducer;
+   ubyte label;
+
+   ubyte blockByteSize;
+
+   string applicationIdentifier;
+   ubyte[3] authCode;
+
+   ubyte[] data;
+}
+
+struct TableBasedImageBlock
+{
+   ushort imageSeparator;
+
+   ushort left;
+   ushort top;
+   ushort width;
+   ushort height;
+
+   bool localColorTableFlag;
+   bool interlaceFlag;
+   bool sortFlag;
+   ubyte reserved;
+   ubyte localColorTableSize;
+
+   ColorTableBlock colorTable;
+
+   ubyte blocksRead;
+}
+
 /+
    Gradually reads through given file and calls back as it encounters sections
    of the GIF.
 
    GIF89a grammar excerpt:
+   (http://www.w3.org/Graphics/GIF/spec-gif89a.txt)
 
    <GIF Data Stream> ::=     Header <Logical Screen> <Data>* Trailer
 
@@ -104,23 +149,74 @@ class GifReader
 
       bool didReadBlock;
 
-      /+
       do
       {
          didReadBlock = this.ReadNextBlock();
       } while (didReadBlock);
-      +/
-
-      this.ReadGraphicsControlExtension();
    }
 
    private bool ReadNextBlock()
    {
+      ubyte[1] blockPreamble;
+      size_t position = this.input.tell();
+      this.input.rawRead(blockPreamble);
+      ubyte nextByte = blockPreamble[0];
+
+      if (nextByte == ImageSeparator)
+      {
+         this.input.seek(position);
+         this.ReadTableBasedImage();
+         return true;
+      }
+      else if (nextByte == ExtensionIntroducer)
+      {
+         ubyte[1] extensionLabel;
+         this.input.rawRead(extensionLabel);
+         ubyte label = extensionLabel[0];
+
+         switch (label)
+         {
+            case GraphicControlLabel:
+            this.input.seek(position);
+            this.ReadGraphicsControlExtension();
+            return true;
+            break;
+
+            case ApplicationExtensionLabel:
+            this.input.seek(position);
+            this.ReadApplicationExtension();
+            return true;
+            break;
+
+            case CommentLabel:
+            this.input.seek(position);
+            writefln("HRRRRR CommentLabel");
+            return true;
+            break;
+
+            case PlainTextLabel:
+            this.input.seek(position);
+            writefln("HRRRRR PlainTextLabel");
+            return true;
+            break;
+
+            default:
+            writefln("Unexpected extension label 0x%X", label);
+            return false;
+            break;
+         }
+      }
+      else if (nextByte == TrailerByte)
+      {
+         writefln("Trailer byte.");
+         return false;
+      }
+
       return false;
    }
 
    private HeaderBlock* header;
-   public GlobalColorTableBlock* globalColorTable;
+   public ColorTableBlock* globalColorTable;
 
    private void ReadHeader()
    {
@@ -170,8 +266,8 @@ class GifReader
 
       size_t start = this.input.tell();
 
-      GlobalColorTableBlock globalColorTable;
-      globalColorTable.table.length = 3 * this.header.GlobalColorCount;
+      ColorTableBlock globalColorTable;
+      globalColorTable.table.length = 3 * ColorCount(this.header.colorTableSize);
       this.input.rawRead(globalColorTable.table);
 
       size_t end = this.input.tell();
@@ -209,14 +305,111 @@ class GifReader
        &extension);
    }
 
+   private void ReadApplicationExtension()
+   {
+      ApplicationExtensionBlock extension;
+
+      size_t start = this.input.tell();
+
+      ubyte[14] data;
+      this.input.rawRead(data);
+
+      extension.extensionIntroducer = data[0];
+      extension.label = data[1];
+      extension.blockByteSize = data[2];
+
+      extension.applicationIdentifier = cast(string)(data[3..11]);
+      extension.authCode = data[11..14];
+
+      while (true)
+      {
+         ubyte[1] dataChunk;
+         this.input.rawRead(dataChunk);
+         ubyte blockSize = dataChunk[0];
+
+         if (blockSize == 0)
+         {
+            // End of stream.
+            break;
+         }
+
+         auto block = new ubyte[blockSize];
+         this.input.rawRead(block);
+         extension.data ~= block;
+      }
+
+      size_t end = this.input.tell();
+
+      this.callback(GifBlockType.ApplicationExtension, start, end,
+       &extension);
+   }
+
+   private void ReadTableBasedImage()
+   {
+      TableBasedImageBlock block;
+
+      ubyte[10] data;
+      size_t start = this.input.tell();
+      this.input.rawRead(data);
+      block.imageSeparator = data[0];
+
+      block.left = data[1] | data[2] << 8;
+      block.top = data[3] | data[4] << 8;
+      block.width = data[5] | data[6] << 8;
+      block.height = data[7] | data[8] << 8;
+
+      ubyte packed = data[9];
+      block.localColorTableFlag = (packed & 0b10000000) >> 7;
+      block.interlaceFlag =       (packed & 0b01000000) >> 6;
+      block.sortFlag =            (packed & 0b00100000) >> 5;
+      block.reserved =            (packed & 0b00011000) >> 3;
+      block.localColorTableSize = (packed & 0b00000111);
+
+      if (block.localColorTableFlag)
+      {
+         block.colorTable.table.length = 3 *
+          ColorCount(block.localColorTableSize);
+
+         this.input.rawRead(block.colorTable.table);
+      }
+
+      // Now we're at the image data blocks. Just fast-forward past them for
+      // now.
+
+      ubyte[1] minimumCodeSize;
+      this.input.rawRead(minimumCodeSize);
+
+      block.blocksRead = 0;
+
+      while (true)
+      {
+         ubyte[1] blockSize;
+         this.input.rawRead(blockSize);
+
+         if (blockSize[0] == 0)
+         {
+            break;
+         }
+
+         auto dataBlock = new ubyte[blockSize[0]];
+         this.input.rawRead(dataBlock);
+         block.blocksRead++;
+      }
+
+      size_t end = this.input.tell();
+
+      this.callback(GifBlockType.TableBasedImage, start, end, &block);
+   }
+
    @property
    public uint[] GlobalColors()
    {
       uint[] colors;
 
       auto table = this.globalColorTable.table;
+      uint colorCount = ColorCount(this.header.colorTableSize);
 
-      for (int i = 0; i < this.header.GlobalColorCount * 3; i += 3)
+      for (int i = 0; i < colorCount * 3; i += 3)
       {
          ubyte red = table[i + 0];
          ubyte green = table[i + 1];
@@ -252,9 +445,10 @@ void FoundBlock(GifBlockType blockType, size_t start, size_t end, void* data)
        header.colorResolution + 1);
       writefln("Sort flag: %d", header.sortFlag);
       writefln("Global color table size: %d (%d)",
-       header.colorTableSize, header.GlobalColorCount);
+       header.colorTableSize, ColorCount(header.colorTableSize));
       writefln("BG color index: %d", header.bgColorIndex);
       writefln("Pixel aspect ratio: %d", header.aspectRatio);
+      writeln();
       break;
 
       case GifBlockType.GlobalColorTable:
@@ -274,6 +468,37 @@ void FoundBlock(GifBlockType blockType, size_t start, size_t end, void* data)
       writefln("Delay time: %d", block.delayTime);
       writefln("Transparent color index: %d", block.transparentColorIndex);
       writefln("Block teminator: %d", block.blockTerminator);
+      writeln();
+      break;
+
+      case GifBlockType.ApplicationExtension:
+      auto block = cast(ApplicationExtensionBlock*)(data);
+      writefln("\nExtension intro: 0x%X", block.extensionIntroducer);
+      writefln("Label: 0x%X", block.label);
+      writefln("Block byte size: %d", block.blockByteSize);
+
+      writefln("Application identifier: '%s'", block.applicationIdentifier);
+      writefln("Auth code: '%s'", cast(string)(block.authCode));
+      writefln("Data length: %d", block.data.length);
+      writeln();
+      break;
+
+      case GifBlockType.TableBasedImage:
+      auto block = cast(TableBasedImageBlock*)(data);
+
+      writefln("\nImage separator: 0x%X", block.imageSeparator);
+      writefln("Image position: %d, %d", block.left, block.top);
+      writefln("Image size: %d x %d", block.width, block.height);
+
+      writefln("Local color table flag: %d", block.localColorTableFlag);
+      writefln("Interlace flag: %d", block.interlaceFlag);
+      writefln("Sort flag: %d", block.sortFlag);
+      writefln("'reserved': %d", block.reserved);
+      writefln("Local color table size: %d", block.localColorTableSize);
+
+      writefln("Image data blocks read: %d", block.blocksRead);
+      writeln();
+
       break;
 
       default:
